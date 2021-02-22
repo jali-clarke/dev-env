87e5ad6b42518f51f900d982bfe8a8cd67ce02cd
jali-clarke <jinnah.ali-clarke@outlook.com>
2021-02-22 19:16:35 +0000

    add secrets decryption (yay!)

--- k8s-manifests/default.nix
+++ k8s-manifests/default.nix
@@ -2,10 +2,20 @@
 let
   volumesAndNamespaceManifest = import ./dev-env-volumes.nix {inherit pkgs;};
   deploymentManifest = import ./dev-env-deployment.nix {inherit pkgs imageNameWithTag deploymentEnv;};
+
+  ccat = "${pkgs.ccrypt}/bin/ccat";
+  kubectl = "${pkgs.kubectl}/bin/kubectl";
 in
 pkgs.writeScriptBin "apply_manifests" ''
   #!${pkgs.runtimeShell} -xe
 
-  ${pkgs.kubectl}/bin/kubectl apply -f ${volumesAndNamespaceManifest}
-  ${pkgs.kubectl}/bin/kubectl apply -f ${deploymentManifest}
+  if [ -z "$SECRETS_PASSPHRASE" ]; then
+    echo "please set env var SECRETS_PASSPHRASE"
+    exit 1
+  fi
+
+  ${ccat} -E SECRETS_PASSPHRASE -c ${./dev_env_secrets.yaml.cpt} | ${kubectl} apply -f -
+
+  ${kubectl} apply -f ${volumesAndNamespaceManifest}
+  ${kubectl} apply -f ${deploymentManifest}
 ''

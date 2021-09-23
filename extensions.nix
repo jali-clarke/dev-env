5a5722ace3fb3cd154a44dda29de5a9f4cef0ecd
jali-clarke <jinnah.ali-clarke@outlook.com>
2021-09-23 14:27:57 +0000

    use vscode-extensions

--- extensions.nix
+++ extensions.nix
@@ -2,85 +2,18 @@
 let
   lib = pkgs.lib;
   stdenv = pkgs.stdenv;
-
-  codeServerExt = { extensionName, version, publisher, hash, galleryUrl ? "https://marketplace.visualstudio.com/_apis/public/gallery" }:
-    let
-      url = "${galleryUrl}/publishers/${publisher}/vsextensions/${extensionName}/${version}/vspackage";
-      extDirName = lib.toLower "${publisher}.${extensionName}-${version}";
-      extFileName = "${extDirName}.vsix";
-
-      self = stdenv.mkDerivation {
-        name = "code-server-ext.${extDirName}";
-        nativeBuildInputs = [
-          pkgs.cacert
-          pkgs.wget
-          pkgs.libarchive
-        ];
-
-        passthru = {
-          inherit extDirName;
-          extDirPath = "${self}/${extDirName}";
-        };
-
-        src = pkgs.fetchurl {
-          inherit hash url;
-          name = extFileName;
-        };
-
-        unpackPhase = ''
-          bsdtar -xf $src
-        '';
-
-        dontBuild = true;
-
-        installPhase = ''
-          mkdir -p $out
-          mv extension $out/${extDirName}
-        '';
-      };
-    in
-    self;
+  exts = pkgs.vscode-extensions;
 
   codeServerExts = [
-    (
-      codeServerExt {
-        extensionName = "Nix";
-        version = "1.0.1";
-        publisher = "bbenoist";
-        hash = "sha256-+zoR6EupIkKiWOdcW8FxTaJbWjjZVE0G+mB6S5qAmEw=";
-      }
-    )
-    (
-      codeServerExt {
-        extensionName = "language-haskell";
-        version = "3.3.0";
-        publisher = "justusadam";
-        hash = "sha256-8lwbKJKG+pbbZxfwg9oDr15TM6Ed4eecAGXHTc7P9/w=";
-      }
-    )
-    (
-      codeServerExt {
-        extensionName = "language-purescript";
-        version = "0.2.4";
-        publisher = "nwolverson";
-        hash = "sha256-ZsHVDtNaqzSTzuyZ8dMKg9VSdNHis4g9HO7KMCmKzDI=";
-      }
-    )
-    (
-      codeServerExt {
-        extensionName = "toml";
-        version = "0.5.1";
-        publisher = "be5invis";
-        hash = "sha256-QwaFTGsBugrViiht4kRtLyM1ebUvG+xh6Mw3Ftd/gXI=";
-      }
-    )
+    exts.bbenoist.Nix
+    exts.justusadam.language-haskell
   ];
 in
 pkgs.runCommand "dev-env-code-server-exts" { } ''
     mkdir -p $out/extensions
     ${lib.concatMapStrings
   (extDerivation: ''
-      ln -s ${extDerivation.extDirPath} $out/extensions/${extDerivation.extDirName}
+      ln -s ${extDerivation}/share/vscode/extensions/${extDerivation.vscodeExtUniqueId} $out/extensions/${extDerivation.vscodeExtUniqueId}
     '')
   codeServerExts}
 ''

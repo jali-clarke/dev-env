d45031cdbbf16d5ddcff9430641058746d26e94d
jali-clarke <jinnah.ali-clarke@outlook.com>
2022-01-02 05:14:18 +0000

    add git aliases

--- config-files.nix
+++ config-files.nix
@@ -75,6 +75,16 @@ usersFiles ++ [
         rebase = false
       [init]
         defaultBranch = master
+      [alias]
+        autosquash = !GIT_SEQUENCE_EDITOR=true git rebase -i --autosquash
+        branchc = branch --show-current
+        brancho = !echo origin/$(git branchc)
+        diffc = !git diff $1~1
+        diffo = !git diff $(git brancho)
+        fixup = commit --fixup
+        fixupa = commit -a --fixup
+        pushf = push --force-with-lease
+        pushuo = !git push -u origin $(git branchc)
     ''
   )
   (

48f9f37f2d62004c4fc0e30ef4f9bb0ba2bddbda
jali-clarke <jinnah.ali-clarke@outlook.com>
2022-01-07 16:35:23 +0000

    typo

--- deployer.nix
+++ deployer.nix
@@ -2,7 +2,7 @@
 let
   git = "${pkgs.git}/bin/git";
   artifactPusher = import ./artifact-pusher.nix { inherit pkgs dev-env-image; };
-  image-patch-json-2902 = pkgs.writeText "image-patch.json6902.yaml" ''
+  image-patch-json-6902 = pkgs.writeText "image-patch.json6902.yaml" ''
     - op: replace
       path: /spec/template/spec/containers/0/image
       value: "${dev-env-image.imageNameWithTag}"
@@ -20,7 +20,7 @@ pkgs.writeShellScriptBin "deploy_dev_env" ''
   pushd ${homelab-config-path}
   ${git} checkout master
   rm ${patchPathInRepo}
-  cp ${image-patch-json-2902} ${patchPathInRepo}
+  cp ${image-patch-json-6902} ${patchPathInRepo}
   ${git} add ${patchPathInRepo}
   ${git} commit -m "updated image to ${dev-env-image.imageNameWithTag}"
   ${git} push
